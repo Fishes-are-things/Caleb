ebbc13322c166d96adffc3d009e613e120f6ccab
Fishes-are-things <146807213+Fishes-are-things@users.noreply.github.com>
2023-10-08 17:36:14 +0800

    iwave.py

--- iwave.py
+++ iwave.py
@@ -13,8 +13,8 @@ servo2 = GPIO.PWM(12,50) # pin 12 for servo2
 
 print("Wave maker starting")
  # Start PWM running on both servos, value of 0 (pulse off)
-  servo1.start(0)
-  servo2.start(0)
+servo1.start(0)
+servo2.start(0)
 
 while True:
 
